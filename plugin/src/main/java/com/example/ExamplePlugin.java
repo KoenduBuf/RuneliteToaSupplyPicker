704f9960ca108aab03bc86ba13cefbfc3aed5e79
KoenduBuf <k.a.p.dubuf@student.tudelft.nl>
2024-10-22 00:20:58 +0200

    Fix merge

--- plugin/src/main/java/com/example/ExamplePlugin.java
+++ plugin/src/main/java/com/example/ExamplePlugin.java
@@ -1,16 +1,21 @@
 package com.example;
 
+import com.google.gson.Gson;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.VarbitChanged;
+import net.runelite.api.events.WidgetLoaded;
+import net.runelite.api.widgets.Widget;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import okhttp3.OkHttpClient;
 
 import java.util.*;
 
