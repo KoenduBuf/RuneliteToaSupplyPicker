3214a6ae15cfc388ab601ee4ab49bd768fdef6ea
KoenduBuf <k.a.p.dubuf@student.tudelft.nl>
2024-10-22 00:17:13 +0200

    Added most of the code for server requests

--- plugin/src/main/java/com/example/ExamplePlugin.java
+++ plugin/src/main/java/com/example/ExamplePlugin.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.google.gson.Gson;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
@@ -7,10 +8,14 @@ import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.events.VarbitChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import okhttp3.OkHttpClient;
 
 @Slf4j
 @PluginDescriptor(
@@ -22,27 +27,45 @@ public class ExamplePlugin extends Plugin
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	public ExampleConfig config;
+
+	@Inject
+	public OkHttpClient okHttpClient;
+
+	@Inject
+	public Gson gson;
+
+	public PartyChecker partyChecker;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
-	}
-
-	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Example stopped!");
+		partyChecker = new PartyChecker();
 	}
 
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		if (gameStateChanged.getGameState() == GameState.LOADING)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			int r = partyChecker.getCurrentRegionId(client);
+			log.info("[TOA SUPPLY CHECKER] Loading finished: " + r);
 		}
+	}
+
+	@Subscribe
+	public void onVarbitChanged(VarbitChanged event) {
+		partyChecker.update(client);
+	}
+
+	@Subscribe
+	public void onConfigChanged(final ConfigChanged event)
+	{
+//		if (!event.getGroup().equals(ExampleConfig.CONFIG_GROUP)) {
+//			return;
+//		}
+
+		new SupplyRequest().performRequest(client, this);
 	}
 
 	@Provides
